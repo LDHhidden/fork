46ba7fc5a3b93ae487d57c35abc71353c976e7c1
LDHhidden <ekgudsl5434@naver.com>
2024-04-26 11:34:17 +0900

    lee1.5

--- fork/static/js/view.js
+++ fork/static/js/view.js
@@ -5,3 +5,13 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+document.getElementById("button-search").addEventListener("click", function() {
+    // GET 또는 INPUT값을 통해서 받아야함
+    var searchTerm = document.getElementById("searchInput").value;
+    // 특정 검색어가 넣어야 동작
+    if (searchTerm === "만약예 예를 들면 DB를 조회한다던가") {
+        // 예시 -> 수정하셈  document.getElementById("elementId").style.color = "red";
+    } else {
+        // 예시 -> 수정하셈 document.getElementById("elementId").style.color = "black";
+    }
+});
